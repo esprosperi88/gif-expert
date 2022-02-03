e4efd201e408358c98faed2e60a74818c99a0af5
efp <efp246@gmail.com>
2022-02-03 14:45:47 +0100

    test use

--- src/tests/hooks/useFetchGifs.test.js
+++ src/tests/hooks/useFetchGifs.test.js
@@ -2,14 +2,33 @@
 import  {useFetchGifs} from '../../hooks/useFetchGifs';
 import {renderHook} from '@testing-library/react-hooks';
 
-
-test('useFetchGifs - debe retornar el estado inicial',() => {
+describe('Prueba component - useFetchGifs ',() => {
     
-    const {result} = renderHook(()=>useFetchGifs('Dragon'));
+    test('useFetchGifs - debe retornar el estado inicial',async() => {
+    
+        const {result,waitForNextUpdate} = renderHook(()=>useFetchGifs('Dragon'));
+        const {data,loading} = result.current;
+        await waitForNextUpdate();
+        
+        expect(data).toEqual([]);
+        expect(loading).toBe(true);
+    
+    });
+    
+    test('useFetchGifs - debe cantidad de 10 imagenes', async() => {
+    
+        const {result,waitForNextUpdate} = renderHook(()=>useFetchGifs('Dragon'));
+        await waitForNextUpdate();  
+        const {data,loading} = result.current;
 
-    const {data,loading} = result.current;
+  
+        expect(data.length).toBe(10);
+        expect(loading).toBe(false);
+    
+    });
+    
+        
 
-    expect(data).toEqual([]);
-    expect(loading).toBe(true);
 
-})
+
+});
